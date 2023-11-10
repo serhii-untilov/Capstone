ebf6464f3b494a8652d10c09ed61bd30560b5e5b
Sergey Untilov <s.untilov@gmail.com>
2023-11-10 18:38:47 +0200

    React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,8 @@
 import React, { Component } from "react";
 import Sidenav from "./components/Sidenav"
 import axios from 'axios';
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route
-} from "react-router-dom";
-import Login from "./components/Login"
-import Register from "./components/Register"
-import Company from "./components/Company"
-import RegisterCompany from "./components/RegisterCompany"
-import Profile from "./components/Profile"
-import Dashboard from "./components/Dashboard"
-import Staff from "./components/Staff"
-import Employees from "./components/Employees"
-import PayrollSheet from "./components/PayrollSheet"
-import PersonalCard from "./components/PersonalCard"
-import Payroll from "./components/Payroll"
-import Settings from "./components/Settings"
-import Language from "./components/Language"
-import Logout from "./components/Logout"
-import Home from "./components/Home"
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "./Routes"
 
 class App extends Component {
 
@@ -175,24 +157,7 @@ class App extends Component {
 					<div className="row h-100 w-100">
 						<Sidenav className="col-sm-4 col-lg-2" />
 						<div className="col-sm-8 col-lg-10">
-							<Switch>
-								{/* <Route path="/login"><Login /></Route> */}
-								<Route path="/login" exact component={Login} />
-								<Route path="/register" exact component={Register} />
-								<Route path="/company" exact component={Company} />
-								<Route path="/register-company" exact component={RegisterCompany} />
-								<Route path="/profile" exact component={Profile} />
-								<Route path="/dashboard" exact component={Dashboard} />
-								<Route path="/staff" exact component={Staff} />
-								<Route path="/employees" exact component={Employees} />
-								<Route path="/payroll-sheet" exact component={PayrollSheet} />
-								<Route path="/personal-card" exact component={PersonalCard} />
-								<Route path="/payroll" exact component={Payroll} />
-								<Route path="/settings" exact component={Settings} />
-								<Route path="/language" exact component={Language} />
-								<Route path="/logout" exact component={Logout} />
-								<Route path="/" exact component={Home} />
-							</Switch>
+							<Routes />
 						</div>
 					</div>
 				</main>
